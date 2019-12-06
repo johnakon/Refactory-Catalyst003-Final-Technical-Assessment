2db8c88c23c36998a48cf3e21c8f20ef7b825bac
EmmellaBeatrice <danahbeat@gmail.com>
2019-12-06 06:22:55 -0800

    working on the form assessment

--- AKONYA_JOHN/index.js
+++ AKONYA_JOHN/index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 // Mongoose connects the app to the mongoDB.
-mongoose.connect("mongodb://localhost:27017/ProfileMe",() => {
+mongoose.connect("mongodb://localhost:27017/refactory",() => {
   console.log("connected successfully to databse")
 });
 
@@ -23,7 +23,6 @@ mongoose.connect("mongodb://localhost:27017/ProfileMe",() => {
 const userSchema = new mongoose.Schema({
   surname:{ type:String, unique : true, required : "surname required"},
   givenname:{ type:String, unique : true, required : "Lastname required"},
-  password:{ type:String, required : "given name required"},
   gender:{ type:String, required : "gender required"},
   country:{ type:String, required : "country required"},
   placeofResidence:{ type:String, required : "place of residence required"},
@@ -35,17 +34,30 @@ const userSchema = new mongoose.Schema({
 })
 
 //creating the model for this schema
-const userModel  = mongoose.model("user", userSchema)
+const myProfileModel  = mongoose.model("student", userSchema)
 
 
-// routes
-app.get('/', (req, res, next) => {
+
+
+// ROUTES
+app.get('/', (req, res) => {
   res.render('myForm', {title : 'ProfileMe'})
-})
+});
 
+// server.post('/register', (req, res) => {
+//     res.send("we are live")
+// });
+//get data from the form
+app.post('/mydata', (req, res) => {
+  // console.log(req.body.firstname, req.body.password);
+
+  //creating an instance of the data entered
+  let newProfileModel = new myProfileModel(req.body)
+  newProfileModel.save()
+});
 
 
 //Setting the server and to Listen to port 3000
-app.listen(3000, () => {
-    console.log("listening on 3000");
+app.listen(2000, () => {
+    console.log("listening on 2000");
   });
